2a15b3acd28cd74fd453bf59f6583c3460561bcb
TannerMarshall551 <tmarsha1@stevens.edu>
2024-01-31 18:59:08 -0500

    Implemented the ability to pickup and drop objects using left click
    Prefab of Player has new script. Package prefab has the script in it to be grabbable. If you are making a new object that you want to be picked up just add the ObjectGrabbable script to it.

---
+++ Assets/Scripts/ObjectGrabbable.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectGrabbable : MonoBehaviour
+{
+    private Rigidbody objectRigidBody;
+    private Transform objectGrabPointTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Grab(Transform objectGrabPointTransform)
+    {
+        this.objectGrabPointTransform = objectGrabPointTransform;
+        objectRigidBody.useGravity = false;
+    }
+
+    public void Drop()
+    {
+        this.objectGrabPointTransform = null;
+        objectRigidBody.useGravity = true;
+    }
+
+    private void Awake()
+    {
+        objectRigidBody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        if(objectGrabPointTransform != null) 
+        {
+            float lerpSpeed = 10f;
+            Vector3 newPosition = Vector3.Lerp(transform.position, objectGrabPointTransform.position, Time.deltaTime * lerpSpeed);
+            objectRigidBody.MovePosition(newPosition);
+        }
+    }
+}
+
+
