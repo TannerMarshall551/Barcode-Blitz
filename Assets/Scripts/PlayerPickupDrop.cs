6769de152289583114ab42c8f95fd9638572bbed
TannerMarshall551 <tmarsha1@stevens.edu>
2024-02-01 11:28:33 -0500

    Updated PlayerPickupDrop so player can use Q to pickup and drop items

--- Assets/Scripts/PlayerPickupDrop.cs
+++ Assets/Scripts/PlayerPickupDrop.cs
@@ -12,7 +12,7 @@ public class PlayerPickupDrop : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Q))
         {
             if (objectGrabbable == null)
             {
