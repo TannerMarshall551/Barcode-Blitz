225e434478396607a553354337bfbbb0e14b9880
TannerMarshall551 <126819417+TannerMarshall551@users.noreply.github.com>
2024-02-15 17:16:52 -0500

    Fixed rotation because merge was broken

---
+++ Assets/Scripts/SmoothRotation.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothRotation : MonoBehaviour
+{
+    private PlayerPickupDrop playerPickupDrop;
+    public bool isHolding;
+    public bool rotationModeActive;
+
+    private GameObject objectToRotate;
+
+    Quaternion targetRotation;
+
+    private void Awake()
+    {
+        playerPickupDrop = GetComponent<PlayerPickupDrop>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        objectToRotate = GetComponent<PlayerPickupDrop>().boxBeingHeld;
+
+        isHolding = playerPickupDrop.isHolding;
+        if (Input.GetKeyDown(KeyCode.R) && isHolding)
+        {
+            if (rotationModeActive)
+                rotationModeActive = false;
+            else
+            {
+                rotationModeActive = true;
+                objectToRotate.transform.rotation = Quaternion.Euler(0f, objectToRotate.transform.eulerAngles.y, 0f);
+            }
+        }
+
+        if (!isHolding)
+        {
+            rotationModeActive = false;
+        }
+
+        if (rotationModeActive)
+        {
+             if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+             {
+                 RotateUp();
+             }
+            if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                RotateLeft();
+            }
+            else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                RotateDown();
+            }
+            else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                RotateRight();
+            }
+        }
+    }
+
+    private void RotateLeft()
+    {
+        targetRotation = Quaternion.Euler(objectToRotate.transform.eulerAngles.x, objectToRotate.transform.eulerAngles.y + 90, objectToRotate.transform.eulerAngles.z);
+
+        objectToRotate.transform.rotation = targetRotation;
+    }
+
+    private void RotateRight()
+    {
+        targetRotation = Quaternion.Euler(objectToRotate.transform.eulerAngles.x, objectToRotate.transform.eulerAngles.y - 90, objectToRotate.transform.eulerAngles.z);
+
+        objectToRotate.transform.rotation = targetRotation;
+    }
+
+    private void RotateUp()
+    {
+    targetRotation = Quaternion.Euler(objectToRotate.transform.eulerAngles.x + 90, objectToRotate.transform.eulerAngles.y, 0f);
+
+    objectToRotate.transform.rotation = targetRotation;
+    }
+
+    private void RotateDown()
+    {
+    targetRotation = Quaternion.Euler(objectToRotate.transform.eulerAngles.x - 90, objectToRotate.transform.eulerAngles.y, 0f);
+
+    objectToRotate.transform.rotation = targetRotation;
+    }
+}
