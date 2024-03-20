04eded93afbaae449e853cee0b9702654e68f3a1
TannerMarshall551 <126819417+TannerMarshall551@users.noreply.github.com>
2024-03-20 18:18:43 -0400

    Created prefabs with cars and fixed null reference error in spawn manager
    Co-Authored-By: ellenan2 <114698697+ellenan2@users.noreply.github.com>

--- Assets/Scripts/ReturnsProcessorSpawnManager.cs
+++ Assets/Scripts/ReturnsProcessorSpawnManager.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class ReturnsProcessorSpawnManager : MonoBehaviour
 {
-    public GameObject package, smallPackage, bigPackage;
+    public GameObject package;
     public GameObject conveyorPoint;
 
     public float spawnRate = 2f;
 
     float nextSpawn = 0f;
 
-    int whatToSpawn;
-    GameObject newBox;
+    private GameObject newBox;
     public float speed;
     private bool arrived;
 
@@ -28,38 +27,29 @@ public class ReturnsProcessorSpawnManager : MonoBehaviour
     {
         if(Time.time > nextSpawn)
         {
-            whatToSpawn = Random.Range(1, 4);
-
-            switch(whatToSpawn)
-            {
-                case 1:
-                    newBox = (GameObject)GameObject.Instantiate(smallPackage, transform.position, Quaternion.identity);
-                    arrived = false;
-                    break;
-                case 2:
-                    newBox = (GameObject)GameObject.Instantiate(package, transform.position, Quaternion.identity);
-                    arrived = false;
-                    break;
-                case 3:
-                    newBox = (GameObject)GameObject.Instantiate(bigPackage, transform.position, Quaternion.identity);
-                    arrived = false;
-                    break;
-            }
+  
+            newBox = (GameObject)GameObject.Instantiate(package, transform.position, Quaternion.identity);
+            arrived = false;
+             
             ObjectGrabbable objRemove = newBox.GetComponent<ObjectGrabbable>();
             if (objRemove != null)
                 Destroy(objRemove);
 
             nextSpawn = Time.time + spawnRate;
         }
-        if (newBox.transform.position.x > 0 && !arrived)
+        if(newBox != null)
         {
-            newBox.transform.position = Vector3.MoveTowards(newBox.transform.position, conveyorPoint.transform.position, speed * Time.deltaTime);
+            if (newBox.transform.position.x > 0 && !arrived)
+            {
+                newBox.transform.position = Vector3.MoveTowards(newBox.transform.position, conveyorPoint.transform.position, speed * Time.deltaTime);
+            }
+            else
+            {
+                arrived = true;
+                newBox.AddComponent<ObjectGrabbable>();
+            }
         }
-        else
-        {
-            arrived = true;
-            newBox.AddComponent<ObjectGrabbable>();
-        }
+        
         
     }
 }
