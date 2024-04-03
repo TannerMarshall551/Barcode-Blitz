0a0a312f15cc948ca04697fd41dbd1278ae31cdb
therman77 <127142043+therman77@users.noreply.github.com>
2024-04-02 21:25:40 -0400

    Added partial game loop functionality; cart & conveyor belt

---
+++ Assets/Scripts/CartGrabbable.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+
+public class CartGrabbable : MonoBehaviour
+{
+    private GameObject player;
+    private Rigidbody cartRigidbody;
+    private bool isGrabbed = false;
+
+    void Awake()
+    {
+        player = GameObject.Find("Player");
+        cartRigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                // Only allow cart to be grabbed by handlebar
+                if (hit.collider.CompareTag("Handlebar"))
+                {
+                    isGrabbed = !isGrabbed;
+                    if (isGrabbed)
+                    {
+                        cartRigidbody.isKinematic = true;
+                    }
+                    else
+                    {
+                        cartRigidbody.isKinematic = false;
+                    }
+                }
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (isGrabbed)
+        {
+            // Snap cart in front
+            Vector3 targetPosition = player.transform.position + player.transform.forward * 1;
+
+            // Center cart
+            targetPosition += player.transform.right;
+
+            // Keep cart on ground
+            targetPosition.y = cartRigidbody.position.y;
+            cartRigidbody.MovePosition(Vector3.Lerp(transform.position, targetPosition, Time.fixedDeltaTime * 10f));
+
+            // Rotate cart with player rotation
+            Quaternion targetRotation = Quaternion.LookRotation(player.transform.forward);
+            cartRigidbody.MoveRotation(Quaternion.Slerp(cartRigidbody.rotation, targetRotation, Time.fixedDeltaTime * 10f));
+        }
+    }
+
+}
---
+++ Assets/Scripts/PlaceholderSnapping.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+
+public class PlaceholderSnapping : MonoBehaviour
+{
+    private GameObject snappedPackage = null;
+
+    private void OnCollisionStay(Collision collision)
+    {
+        // collision.gameObject.CompareTag("MediumPackage")
+        if (collision.gameObject.tag.Equals(this.tag) && snappedPackage == null)
+        {
+            snappedPackage = collision.gameObject;
+            Transform placeholderTransform = transform;
+            SnapPackageToPlaceholder(snappedPackage, placeholderTransform);
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag.Equals(this.tag) && snappedPackage != null)
+        {
+            var packageRigidBody = snappedPackage.GetComponent<Rigidbody>();
+            packageRigidBody.constraints = RigidbodyConstraints.None;
+            snappedPackage.transform.SetParent(null);
+            snappedPackage = null;
+        }
+    }
+
+    private void SnapPackageToPlaceholder(GameObject package, Transform placeholder)
+    {
+        Debug.Log("Snapping");
+        var packageBounds = package.GetComponentInChildren<Collider>().bounds;
+
+        // Calculate placeholder size
+        var placeholderBounds = GetComponent<Collider>().bounds;
+        Debug.Log("Placeholder bounds: ");
+        Debug.Log(placeholderBounds);
+        Vector3 placeholderCenterTop = new Vector3(placeholderBounds.center.x, placeholderBounds.max.y, placeholderBounds.center.z);
+
+        // Find current bottom of package
+        float lowestPoint = packageBounds.min.y;
+
+        // Calculate position to snap to
+        Vector3 targetPosition = placeholderCenterTop + (Vector3.up * (packageBounds.extents.y - lowestPoint + placeholderBounds.extents.y));
+
+        // Set position and rotation
+        snappedPackage.transform.position = new Vector3(targetPosition.x, targetPosition.y, targetPosition.z);
+        snappedPackage.transform.rotation = transform.rotation;
+        snappedPackage.transform.SetParent(placeholder, true);
+        var packageRigidBody = package.GetComponent<Rigidbody>();
+        if (packageRigidBody != null)
+        {
+            packageRigidBody.constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (snappedPackage != null)
+        {
+            snappedPackage.transform.rotation = transform.rotation;
+        }
+    }
+}
